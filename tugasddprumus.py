7d4ba353ac7b693d7fb65bd1caf1a4cbffa2db2e
solihin999 <muhamadsolihin0857@gmail.com>
2024-10-10 21:44:29 +0700

    Tugas1 DDP

---
+++ tugasddprumus.py
@@ -0,0 +1,39 @@
+#Rumus Menghitung Luas Persegi
+sisi = 8
+luas_persegi = sisi * sisi
+print(f"Luas persegi dengan sisi {sisi} adalah {luas_persegi}")
+
+#rumus menghitung luas persegi panjang
+panjang = 9
+lebar = 7
+luas_persegipanjang = panjang * lebar
+print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas_persegipanjang}")
+
+#rumus luas segitiga
+alas = 5
+tinggi = 7
+luas_segitiga = 1/2 * alas * tinggi
+print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas_segitiga}")
+
+#rumus menghitung luas lingkaran 
+jari2 = 4
+luas_lingkaran = 3.14 * jari2 * jari2
+print(f"Luas lingkaran dengan jari jari {jari2} adalah {luas_lingkaran}")
+
+#perbandingan lebih besar dan lebih kecil dari luas persegi dan luas persegi panjang
+luas_persegi = 64 
+luas_persegipanjang = 63
+print("Perbandingan Lebih Besar dan Lebih Kecil dari Luas Persegi dan Persegi Panjang :") 
+print(luas_persegi < luas_persegipanjang) 
+print(luas_persegi > luas_persegipanjang) 
+print(luas_persegipanjang < luas_persegi) 
+print(luas_persegipanjang > luas_persegi)
+
+#perbandingan lebih besar dan lebih kecil dari luas segitiga dan lingkarang
+luas_segitiga = 17.5
+luas_lingkaran = 50.24
+print("Perbandingan Lebih Besar dan Lebih Kecil dari Luas Segitiga dan Lingkaran :")
+print(luas_segitiga < luas_lingkaran)
+print(luas_segitiga > luas_lingkaran)
+print(luas_lingkaran < luas_segitiga)
+print(luas_lingkaran > luas_segitiga)
